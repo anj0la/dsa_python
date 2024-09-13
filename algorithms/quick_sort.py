65bdd474b8e22473d6b233d90c10ccfe46eb271e
anj0la <anjola.aina@gmail.com>
2024-09-13 10:41:40 -0600

    Implemented the hoare partitioning scheme for quick sort.

--- algorithms/quick_sort.py
+++ algorithms/quick_sort.py
@@ -1,3 +1,39 @@
+def hoare_partition(arr: list, low: int, high: int) -> int:
+    """
+    Implements the Hoare partition with the pivot as the first element. 
+    This partititoning scheme is the fastest out of three.
+
+    Args:
+        arr (list): The input array.
+        low (int): The starting index of the subarray.
+        high (int): The ending index of the subarray.
+
+    Returns:
+        int: The location of the pivot.
+    """
+    pivot = arr[low] # Selecting the first element as the pivot
+    
+    i = low - 1 # Left index
+    j = high + 1 # Right index
+    
+    while True:
+        
+        # Find an element on the left-hand side that's greater than the pivot
+        i += 1
+        while arr[i] < pivot:
+            i += 1
+            
+        # Find an element on the right hand-side that's smaller than the pivot
+        j -= 1
+        while arr[j] > pivot:
+            j -= 1
+            
+        # If the indices have crossed (left index >= right index), return the right index (j)
+        if i >= j:
+            return j # The pivot index
+        
+        arr[i], arr[j] = arr[j], arr[i]
+
 def lomuto_partition(arr: list, low: int, high: int) -> int:
     """
     Implements the Lomuto partition with the pivot as the last element.
@@ -54,7 +90,7 @@ def naive_partition(arr: list, low: int, high: int) -> int:
     return pivot_index
        
     
-def quick_sort(arr: list, low: int, high: int, chosen_partition: str = 'lomuto') -> None:
+def quick_sort(arr: list, low: int, high: int, chosen_partition: str = 'hoare') -> None:
     """
     Implements the quick sort algorithm. If the chosen partition is not specified,
     the algorithm uses lomuto partitioning.
@@ -67,10 +103,16 @@ def quick_sort(arr: list, low: int, high: int, chosen_partition: str = 'lomuto')
     """
     if low < high:
         if chosen_partition == 'naive':
-            print('Naive')
             pivot_loc = naive_partition(arr, low, high)
-        else: # Lomuto partition
+        elif chosen_partition == 'lomuto': 
             pivot_loc = lomuto_partition(arr, low, high)
-        quick_sort(arr, low, pivot_loc - 1)
-        quick_sort(arr, pivot_loc + 1, high)
+        else: # Hoare paritition
+            pivot_loc = hoare_partition(arr, low, high)
+
+        if chosen_partition == 'hoare': 
+            quick_sort(arr, low, pivot_loc)
+            quick_sort(arr, pivot_loc + 1, high)
+        else: # Naive and Lomuto use the same values
+            quick_sort(arr, low, pivot_loc - 1)
+            quick_sort(arr, pivot_loc + 1, high)
         
