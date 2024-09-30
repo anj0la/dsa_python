16c18dc461938d03f04c1593995b64c2df8155ff
anj0la <anjola.aina@gmail.com>
2024-09-30 10:46:38 -0600

    Implemented an adjacency list and adjacency matrix.

---
+++ dsa_python/data_structures/adjacency_list.py
@@ -0,0 +1,63 @@
+class Graph:
+    def __init__(self, V, is_directed=False):
+        self.V = V
+        self.directed = is_directed
+        self.adj = [[] for _ in range(V)]
+        
+    def vertex_count(self):
+        return self.V
+    
+    def edge_count(self):
+        sum = 0
+        for i in range(len(self.adj)):
+            sum += len(self.adj[i])
+            
+        return sum
+    
+    def vertices(self):
+        vertices = []
+        for i in range(len(self.adj)):
+            vertices.append(i)
+            
+        return vertices
+    
+    def add_edge(self, u, v):
+        if not self.directed:
+            self.adj[u].append(v)
+            self.adj[v].append(u)
+        else:
+            self.adj[u].append(v)
+            
+    def remove_edge(self, u, v):
+        for i in range(len(self.adj[u])):
+            if self.adj[u][i] == v:
+                self.adj[u].pop(i)
+                break
+        if not self.directed:
+            for i in range(len(self.adj[v])):
+                if self.adj[v][i] == u:
+                    self.adj[v].pop(i)
+                    break
+            
+    def print_graph(self):
+        for v in range(self.V):
+            print('Vertex: ' + str(v), end=' ')
+            
+            for x in self.adj[v]:
+                print(' -> ' + str(x), end='')
+                
+            print()
+        
+if __name__ == '__main__':
+    V = 5
+    graph = Graph(V, is_directed=False)
+    print('Trivial Graph (i.e, graph with no edges): ', graph.adj)
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 4)
+    graph.add_edge(1, 2)
+    graph.add_edge(1, 4)
+    graph.add_edge(2, 3)
+    graph.add_edge(3, 4)   
+    print('Vertex Count: ', graph.vertex_count())
+    print('Edge Count: ', graph.edge_count())
+    graph.print_graph()
---
+++ dsa_python/data_structures/adjacency_matrix.py
@@ -0,0 +1,62 @@
+class Graph:
+    def __init__(self, V, is_directed=False):
+        self.V = V
+        self.directed = is_directed
+        self.adj = [[0] * V for _ in range(V)]
+        
+    def vertex_count(self):
+        return self.V
+    
+    def edge_count(self):
+        sum = 0
+        for u in range(self.V):
+            for v in range(self.V):
+                if self.adj[u][v] == 1:
+                    sum += 1
+                    
+        return sum
+    
+    def vertices(self):
+        vertices = []
+        for i in range(self.V):
+            vertices.append(i)
+            
+        return vertices
+    
+    def add_edge(self, u, v):
+        if not self.directed:
+            self.adj[u][v] = 1
+            self.adj[v][u] = 1
+        else:
+            self.adj[u][v] = 1
+            
+    def remove_edge(self, u, v):
+        if not self.directed:
+            self.adj[u][v] = 0
+            self.adj[v][u] = 0
+        else:
+            self.adj[u][v] = 0
+            
+    def print_graph(self):
+        for u in range(self.V):
+            print('Vertex: ' + str(u), end=' ')
+            for v in range(self.V):
+                if self.adj[u][v] == 1:
+                    print(' -> ' + str(v), end='')
+            print()
+
+        
+if __name__ == '__main__':
+    V = 5
+    graph = Graph(V, is_directed=False)
+    print('Trivial Graph (i.e, graph with no edges): ', graph.adj)
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 4)
+    graph.add_edge(1, 2)
+    graph.add_edge(1, 4)
+    graph.add_edge(2, 3)
+    graph.add_edge(3, 4)   
+    print('Vertex Count: ', graph.vertex_count())
+    print('Edge Count: ', graph.edge_count())
+
+    graph.print_graph()
