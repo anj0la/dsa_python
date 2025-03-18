613e2540ae59102e623c2d91fb754b7f76050980
anj0la <anjola.aina@gmail.com>
2025-03-18 11:16:55 -0600

    Added space optimized version of climbing stairs and house robber.

--- dsa_python/algorithms/dynamic_programming/climbing_stairs.py
+++ dsa_python/algorithms/dynamic_programming/climbing_stairs.py
@@ -76,12 +76,43 @@ def climbing_stairs(n: int) -> int:
     
     # bottom-up (tabulatization), top-down (recursion)
     
+def climbing_stairs_optimization(n: int) -> int:
+    """
+    You are climbing a staircase. It takes n steps to reach the top.
+    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+    
+    Example 1:
+    Input: n = 2
+    Output: 2
+    Explanation: There are two ways to climb to the top.
+    1. 1 step + 1 step
+    2. 2 steps
+    Example 2:
+
+    Input: n = 3
+    Output: 3
+    Explanation: There are three ways to climb to the top.
+    1. 1 step + 1 step + 1 step
+    2. 1 step + 2 steps
+    3. 2 steps + 1 step
+    
+    Constraints:
+
+    1 <= n <= 45
+    """
+    prev = curr = 1
+    for i in range(2, n + 1):
+        prev, curr = prev + curr, prev
+        
+    return prev
     
 if __name__ == '__main__':
-    assert climbing_stairs_recursion(5) == climbing_stairs(5)
-    assert climbing_stairs_recursion(10) == climbing_stairs(10)
+    assert climbing_stairs_recursion(5) == climbing_stairs(5) == climbing_stairs_optimization(5)
+    assert climbing_stairs_recursion(10) == climbing_stairs(10) == climbing_stairs_optimization(10)
 
     print(climbing_stairs_recursion(2))
     print(climbing_stairs(2))
+    print(climbing_stairs_optimization(2))
     print(climbing_stairs_recursion(3))
     print(climbing_stairs(3))
+    print(climbing_stairs_optimization(3))
